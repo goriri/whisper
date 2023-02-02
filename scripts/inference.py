78563e7cf62e0d252f86d378e6d417d9f8ce4aa3
goriri <goriri@163.com>
2023-02-02 14:23:11 +0000

    fix

--- scripts/inference.py
+++ scripts/inference.py
@@ -1,6 +1,9 @@
 import logging
 import json
 import torch
+import librosa
+import numpy as np
+import io
 
 from transformers import WhisperForConditionalGeneration
 from transformers import WhisperProcessor
@@ -17,43 +20,31 @@ logger.info("Libraries are loaded")
 def model_fn(model_dir):
     device = get_device()
 
-    model = WhisperForConditionalGeneration.from_pretrained(model_path).to(device)
-    # model = pipeline(task='automatic-speech-recognition', model=model_dir, device=device)
+    # model = WhisperForConditionalGeneration.from_pretrained(model_path).to(device)
+    model = pipeline(task='automatic-speech-recognition', model=model_dir, device=device)
     logger.info("Model is loaded")
 
     return model
 
 
-def input_fn(json_request_data, content_type='application/json'):
-    input_data = json.loads(json_request_data)
+def input_fn(input_data, content_type):
+    # input_data = json.loads(json_request_data)
     logger.info("Input data is processed")
+    speech_array, sampling_rate = librosa.load(io.BytesIO(input_data), sr=16000)
 
-    return input_data
+    return np.asarray(speech_array)
 
 
 def predict_fn(input_data, model):
     logger.info("Starting inference.")
-    device = get_device()
 
-    logger.info(input_data)
-
-    speech_array = input_data['speech_array']
-    sampling_rate = input_data['sampling_rate']
-
-    processor = WhisperProcessor.from_pretrained(model_path)
-    input_features = processor(speech_array, sampling_rate=sampling_rate, return_tensors="pt").input_features.to(device)
-
-    with torch.no_grad():
-        predicted_ids = model.generate(input_features)
-    transcript = processor.batch_decode(predicted_ids, skip_special_tokens = True)
-
-    # transcript = model(audio)["text"]
+    transcript = model(input_data)["text"]
 
     return transcript
 
 
 def output_fn(transcript, accept='application/json'):
-    return json.dumps(transcript), accept
+    return json.dumps(transcript)
 
 
 def get_device():
